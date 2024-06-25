c9504a6599b266f69547027fb499192a8bc835b4
Bryan DeGrendel <bryan@degrendel.com>
2024-06-25 21:26:02 +0000

    Add HTTP to dev stack
    * Also create all topics in Kafka.
    * Fix very minor typo in the compose base YAML.

--- kafka/bin/init-kafka-topics.sh
+++ kafka/bin/init-kafka-topics.sh
@@ -8,6 +8,12 @@ echo "Waiting for kafka..."
 echo "Creating kafka topics"
 "$KT" --bootstrap-server localhost:9092 --create --if-not-exists --topic messages-input --replication-factor 1 --partitions 1
 "$KT" --bootstrap-server localhost:9092 --create --if-not-exists --topic messages-output --replication-factor 1 --partitions 1
+"$KT" --bootstrap-server localhost:9092 --create --if-not-exists --topic http-input --replication-factor 1 --partitions 1
+"$KT" --bootstrap-server localhost:9092 --create --if-not-exists --topic http-output --replication-factor 1 --partitions 1
+"$KT" --bootstrap-server localhost:9092 --create --if-not-exists --topic discord-input --replication-factor 1 --partitions 1
+"$KT" --bootstrap-server localhost:9092 --create --if-not-exists --topic discord-output --replication-factor 1 --partitions 1
+"$KT" --bootstrap-server localhost:9092 --create --if-not-exists --topic direct-input --replication-factor 1 --partitions 1
+"$KT" --bootstrap-server localhost:9092 --create --if-not-exists --topic direct-output --replication-factor 1 --partitions 1
 
 echo "Successfully created the following topics:"
 "$KT" --bootstrap-server localhost:9092 --list
